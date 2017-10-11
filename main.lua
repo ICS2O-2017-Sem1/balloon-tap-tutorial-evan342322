a16fbf603bed798b559277a5f73dad325c8aa28b
evan342322 <s249125@ocsb.local>
2017-10-11 11:04:07 -0400

    star explorer

--- main.lua
+++ main.lua
@@ -9,3 +9,20 @@ background.x = display.contentCenterX
 background.y = display.contenCeterY
 
 local platform = display.newImageRect( "platform.png", 3oo, 50 )
+platform.x = display.contentCenterX
+platform.y = display.ContentHeight-25
+
+local balloon = dispaly.newImageRect( "balloon.png", 112, 112 )
+baloon.x = display.contentCenterX
+ballon.y = display.contentCenterY
+ballon.alpha = o.8
+
+local physics = require( "physics" )
+physics,start()
+
+physics.addboy( platform, "static" )
+phsics.addboy( balloon, "dynamic", { radius=50, bounce=0.3 } )
+
+local function pushBallon()
+
+end
